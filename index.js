a28ee584f3ab3b0673c6734022ecd493341773ec
Peter Njuguna <Ptahbn@gmail.com>
2022-11-29 10:26:33 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let max = 0;
+    let count = 0;
+    for (let i = 0; i < candles.length; i++) {
+        if (candles[i] > max) {
+            max = candles[i];
+            count = 1;
+        } else if (candles[i] === max) {
+            count++;
+        }
+    }
+    return count;
+}
